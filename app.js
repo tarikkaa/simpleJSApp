324a2f6fb894de1aef1b9016c6f4d97cd2def70f
Taras <testfromtaras@gmail.com>
2022-07-12 23:30:59 +0300

    Lesson 2 home task

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: ");
+//Write a JS program to compute and print the sum of the two given integers
+a = 20;
+b = 30;
+console.log('The sum of '+ a + ' and ' + b + ' is: ' + (a + b));
 
-let stdin = process.openStdin();
+//Write a JS program to create a new string adding “Test_" in front of a given string
+carName = 'Toyota';
+console.log('Test_'+carName);
 
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+//Write a JS program to combine and print your first, last name and age
+firstName = 'John';
+lastNAme = 'Dou';
+age = 22;
+console.log('My first name: ' + firstName + ', last name: ' + lastNAme + ', age: ' + age);
+
+//Write a JS program to check a given integer is bigger than 100
+c = 99;
+console.log(c + ' is bigger than 100: ' + (c > 100));
+
+//Write a JS program to find if the first number is larger from the two given positive integers
+d = 31;
+e = 20;
+f = 30;
+console.log(d + ' is larger than ' + e + ' and ' + f + ': ' + (d > e && d > f));
+
+//Write a JS program to check two given numbers and print true if one of the number is 50 or if their sum is 50
+g = 10;
+h = 50;
+i = g + h;
+console.log('One of the number is 50 or their sum is 50 : ' + (g === 50 || h === 50 || i === 50));
+
+//Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
+myNumber = 9;
+j = 3;
+k = 7;
+console.log(myNumber + ' is multiple of 3: '+ (myNumber % 3 === 0) + '\n' + myNumber + ' is multiple of 7: ' + (myNumber % 7 === 0));

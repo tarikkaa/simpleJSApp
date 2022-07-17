5c13592f73c900607e8a6540e2abcccd6d19d708
Taras <testfromtaras@gmail.com>
2022-07-17 22:47:36 +0300

    let added to variables

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 //Write a JS program to compute and print the sum of the two given integers
-a = 20;
-b = 30;
+let a = 20;
+let b = 30;
 console.log('The sum of '+ a + ' and ' + b + ' is: ' + (a + b));
 
 //Write a JS program to create a new string adding “Test_" in front of a given string
-carName = 'Toyota';
+let carName = 'Toyota';
 console.log('Test_'+carName);
 
 //Write a JS program to combine and print your first, last name and age
-firstName = 'John';
-lastNAme = 'Dou';
-age = 22;
+let firstName = 'John';
+let lastNAme = 'Dou';
+let age = 22;
 console.log('My first name: ' + firstName + ', last name: ' + lastNAme + ', age: ' + age);
 
 //Write a JS program to check a given integer is bigger than 100
-c = 99;
+let c = 99;
 console.log(c + ' is bigger than 100: ' + (c > 100));
 
 //Write a JS program to find if the first number is larger from the two given positive integers
-d = 31;
-e = 20;
-f = 30;
+let d = 31;
+let e = 20;
+let f = 30;
 console.log(d + ' is larger than ' + e + ' and ' + f + ': ' + (d > e && d > f));
 
 //Write a JS program to check two given numbers and print true if one of the number is 50 or if their sum is 50
-g = 10;
-h = 50;
-i = g + h;
+let g = 10;
+let h = 50;
+let i = g + h;
 console.log('One of the number is 50 or their sum is 50 : ' + (g === 50 || h === 50 || i === 50));
 
 //Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
 myNumber = 9;
-j = 3;
-k = 7;
+let j = 3;
+let k = 7;
 console.log(myNumber + ' is multiple of 3: '+ (myNumber % 3 === 0) + '\n' + myNumber + ' is multiple of 7: ' + (myNumber % 7 === 0));

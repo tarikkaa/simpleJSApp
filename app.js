986eeb4febbb70b8c5317551ed578771d9fd6f34
Taras <testfromtaras@gmail.com>
2022-07-19 00:14:25 +0300

    Lesson 3 home task (JS_Basics_2)

--- app.js
+++ app.js
@@ -1,36 +1,66 @@
-//Write a JS program to compute and print the sum of the two given integers
-let a = 20;
-let b = 30;
-console.log('The sum of '+ a + ' and ' + b + ' is: ' + (a + b));
+//Write a JavaScript function to check if given string includes given symbol.
+let string1 = 'Happy new year';
+let myWord = 'new';
+console.log(`This string includes the word "${myWord}":`,string1.includes(myWord));
 
-//Write a JS program to create a new string adding “Test_" in front of a given string
-let carName = 'Toyota';
-console.log('Test_'+carName);
+//Write a JavaScript function to check whether a string is blank or not (string with spaces should be also blank).
 
-//Write a JS program to combine and print your first, last name and age
-let firstName = 'John';
-let lastNAme = 'Dou';
-let age = 22;
-console.log('My first name: ' + firstName + ', last name: ' + lastNAme + ', age: ' + age);
+let string2 = "   "
+let string3 = string2.trim();
+if (string3.length === 0){
+    console.log("The string is blank");
+}
+else{
+    console.log("The sting is not blank");
+}
 
-//Write a JS program to check a given integer is bigger than 100
-let c = 99;
-console.log(c + ' is bigger than 100: ' + (c > 100));
+//Write a JavaScript function to convert a string in abbreviated form. console.log(abbrev (“Nazar Babii")) – should be “N.B.” (should convert lower case names to upper)
 
-//Write a JS program to find if the first number is larger from the two given positive integers
-let d = 31;
-let e = 20;
-let f = 30;
-console.log(d + ' is larger than ' + e + ' and ' + f + ': ' + (d > e && d > f));
+function abbrev (yourName){
+    let firstLetters = yourName.match(/\b(\w)/g);
+    let nameAbbrev = firstLetters.join('.');
+    console.log(`The abbrev of your name is: "${nameAbbrev.toUpperCase()}."`);
+}
+abbrev('Nazar Babii');
 
-//Write a JS program to check two given numbers and print true if one of the number is 50 or if their sum is 50
-let g = 10;
-let h = 50;
-let i = g + h;
-console.log('One of the number is 50 or their sum is 50 : ' + (g === 50 || h === 50 || i === 50));
+//Write a JavaScript function that accept two integers and display the larger.
+let largerNumber = (a, b) => {
+    if (a > b){
+        console.log(`The number ${a} is larger`);
+    }
+    if (a < b){
+        console.log(`The number ${b} is larger`);
+    }
+    else{
+        console.log(`The number ${a} equals ${b}`);
+    }
+}
+largerNumber(10, 10);
 
-//Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
-myNumber = 9;
-let j = 3;
-let k = 7;
-console.log(myNumber + ' is multiple of 3: '+ (myNumber % 3 === 0) + '\n' + myNumber + ' is multiple of 7: ' + (myNumber % 7 === 0));
+//Write a JavaScript function with conditional statement to sort three numbers
+
+function sortThreeNumbers(a, b, c) {
+    if(a < b && a < c && b < c){
+        console.log(`${a}, ${b}, ${c}`);
+    }
+    if(a < b && a < c && c < b){
+        console.log(`${a}, ${c}, ${b}`);
+    }
+    if(b < a && b < c && a < c){
+        console.log(`${b}, ${a}, ${c}`);
+    }
+    if(b < a && b < c && c < a){
+        console.log(`${b}, ${c}, ${a}`);
+    }
+    if(c < a && c < b && a < b){
+        console.log(`${c}, ${a}, ${b}`);  
+    }
+    if(c < a && c < b && b < a){
+        console.log(`${c}, ${b}, ${a}`);
+    }
+    //else{
+    //   console.log('Some of the numbers are equal');
+    //}
+}
+sortThreeNumbers(3, 1, 2);
+
